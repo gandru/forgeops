188c9c4424572f141e0851722bf16bf9a47eb385
Guillaume Andru <guillaume.andru@forgerock.com>
2019-02-25 15:43:27 +0100

    aaaa

--- samples/config/smoke-deployment/env.sh
+++ samples/config/smoke-deployment/env.sh
@@ -1,11 +1,12 @@
 # Environment settings for the deployment
 
 # k8s namespace to deploy in
-NAMESPACE=smoke
+NAMESPACE="gandru"
 
 # Top level domain. Do not include the leading .
-DOMAIN="forgeops.com"
+DOMAIN="gandru.forgeops.com"
 
 # The components to deploy
 # Note the opendj stores are aliased as configstore, userstore, ctstore - but they all use the ds helm chart.
-COMPONENTS=(frconfig configstore userstore ctsstore openam amster postgres-openidm openig openidm web )
+#COMPONENTS=(frconfig configstore userstore ctsstore openam amster postgres-openidm openig openidm web )
+COMPONENTS=(frconfig web openig)
