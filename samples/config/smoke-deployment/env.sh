b8dd6d58b14bf8c15671ccf02c6a9de8f95965a4
Guillaume Andru <guillaume.andru@forgerock.com>
2019-02-25 16:20:33 +0100

    aaa

--- samples/config/smoke-deployment/env.sh
+++ samples/config/smoke-deployment/env.sh
@@ -8,5 +8,5 @@ DOMAIN="gandru.forgeops.com"
 
 # The components to deploy
 # Note the opendj stores are aliased as configstore, userstore, ctstore - but they all use the ds helm chart.
-#COMPONENTS=(frconfig configstore userstore ctsstore openam amster postgres-openidm openig openidm web )
-COMPONENTS=(frconfig web openig)
+COMPONENTS=(frconfig configstore userstore ctsstore openam amster postgres-openidm openig openidm web )
+#COMPONENTS=(frconfig web openig)
